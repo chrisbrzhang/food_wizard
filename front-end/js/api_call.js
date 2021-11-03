0175da0bf1638133219192a418df37e07ac79a67
Chris <zhang.chris89@gmail.com>
2021-11-02 18:06:02 -0700

    appending lis that will contain ingredients

--- front-end/js/api_call.js
+++ front-end/js/api_call.js
@@ -5,17 +5,49 @@ const post = "POST"
 const resource="&ingredients=";
 // test out https://api.spoonacular.com/recipes/findByIngredients?apiKey=API_KEY&ingredients=apples in your url to get json respone
 
-function api_find_by_ingredients(ingredients) {
+function get_recipes() {
 
     let ingredient = document.getElementById('ingredients_area').value;
-
-
-    xhttp.open(get, url +API_KEY + resource+ ingredient, true);
-    xhttp.send();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-            document.getElementById("recipe").innerHTML = this.responseText;
-        }
+    let list = query_ol()
+    console.log(list)
+    if (list.length < 3) {
+        alert("Please insert more ingredients")
+        // return
     }
+    
+    // xhttp.open(get, url +API_KEY + resource+ ingredient, true);
+    // xhttp.send();
+    // xhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         console.log(this.responseText);
+    //         document.getElementById("recipe").innerHTML = this.responseText;
+    //     }
+    // }
+}
+// returns a list of nodes containing all li tags
+function query_ol() {
+    let ol = document.getElementById("list_of_ingredients");
+    let li = ol.querySelectorAll("li");
+    // let list = []
+    // for (var i = 0; i < li.length; i++) {
+    //     console.log(li[i].innerHTML)
+    //     list.push(li[i].innerHTML)
+    // }
+    return li
+}
+
+function add_ingredients() {
+    let ol = document.getElementById("list_of_ingredients");
+    let li = document.createElement("li");
+    let text = document.getElementById('ingredients_area').value.trim();
+    document.getElementById('ingredients_area').value = '';
+   
+    if (text.length == 0) {
+        alert("Field cannot be empty");
+        // gets out of the function
+        return
+    }
+    let word = document.createTextNode(text);
+    li.appendChild(word);
+    ol.append(li);
 }
