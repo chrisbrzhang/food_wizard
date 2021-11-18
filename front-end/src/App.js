821d57802751886dae53127f10e08348b481ab92
unknown <zhang.chris89@gmail.com>
2021-11-17 22:42:42 -0800

    react need to fix functions

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,23 +4,29 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.css'; 
+
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/umd/popper.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.css'; 
+
+import NavBar from './NavBar';
 
 
 // import { BrowserRouter } from 'react-router-dom';
 
-import HomePage from './pages/HomePage'; 
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 
 
 export default function App() {
   return (
     <Router>
+      <NavBar/>
       <div className="App">
         <Switch>
           <Route path="/" component={HomePage}/>
+          <Route path="/about" component={AboutPage}/>
         </Switch>
       </div>
     </Router>
---
+++ front-end/src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import About from "../components/About";
+
+const AboutPage = () => (
+    <React.Fragment>
+    <div className='About'>
+        <h1> About this project </h1>
+    </div>
+    </React.Fragment>
+);
+export default AboutPage;
